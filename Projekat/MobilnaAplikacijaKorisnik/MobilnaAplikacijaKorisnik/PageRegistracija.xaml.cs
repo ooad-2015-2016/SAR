4d94be836fd8c939e2c4c774dddd9e2db2bdc72b
nsljivic1 <nsljivic1@etf.unsa.ba>
2016-04-18 11:59:25 +0200

    Forme aplikacija mobilna/vozac

---
+++ Projekat/MobilnaAplikacijaKorisnik/MobilnaAplikacijaKorisnik/PageRegistracija.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MobilnaAplikacijaKorisnik
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PageRegistracija : Page
+    {
+        public PageRegistracija()
+        {
+            this.InitializeComponent();
+
+        }
+        void GoBack()
+        {
+            if (this.Frame != null && this.Frame.CanGoBack) this.Frame.GoBack();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            GoBack();
+        }
+
+        private void textBlock_Copy_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UputeZaPrijavuTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
